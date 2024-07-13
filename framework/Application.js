4461f97845a50a4d9563485aa7576cc7c7a07ee6
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-07-13 11:04:53 +0200

    Creating Framework

--- framework/Application.js
+++ framework/Application.js
@@ -7,14 +7,22 @@ module.exports = class Application {
         this.server = this._createServer();
     }
 
+    addRouter(router) {
+        
+    }
+
     _createServer() {
         return http.createServer((req, res) => {
-            const emitted = this.emitter.emit(`[${req.url}]:[${req.method}]`, req, res);
+            const emitted = this.emitter.emit(this._getRouteMask(req.utl, req.method), req, res);
             
             if (!emitted) {
                 res.statusCode = 404;
                 res.end('Not Found');
             }
         });
+    }
+
+    _getRouteMask(path, method) {
+        return  `[${req.url}]:[${req.method}]`
     }
 }
